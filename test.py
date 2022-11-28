376f32da022b4771f18b155843f00dfa1709f4fe
darki0ni <116812883+darki0ni@users.noreply.github.com>
2022-11-28 14:26:54 +0100

    Update test.py

--- test.py
+++ test.py
@@ -1,6 +1,18 @@
-sieve = [True]*101
-for i in range (2, 100):
-    if sieve [i]:
-        print(i)
-        for j in range(i*i, 100, i):
-            sieve [j] = False
+import math
+print("Soit une équation de la forme : ax^2 + bx + c = 0")
+a = float(input("a = "))
+b = float(input("b = "))
+c = float(input("c = "))
+delta = b**2 - 4*a*c
+if(delta < 0):
+    print("L'équation n'a pas de solution")
+else:
+    if (delta == 0):
+        print("L'équation a une solution double.")
+        x = -b / (2.*a)
+        print ("La solution est x = ",x)
+    else:
+        print("L'équation a deux solutions.")
+        x1 = (-b - math.sqrt(delta)) /(2*a)
+        x2 = (-b + math.sqrt(delta)) /(2*a)
+        print ("Les solutions sont x1 = ",x1, " et ", x2)
