1b0f77c8f6209ebb400343ee62d3fa001ae1e6a1
darki0ni <alexy412002@gmail.com>
2022-12-04 19:11:58 +0100

    message: carre

--- testons.py
+++ testons.py
@@ -3,20 +3,24 @@ import random
 a = random.randint(1,6)
 b = random.randint(1,6)
 x = str(a) + str(b)
+result = (''.join(sorted(x, reverse=True)))
 
 def nombre1():
-    print('Voici ton nombre: ' +(''.join(sorted(x, reverse=True))))
+    print('Voici ton nombre: ' + result )
     return nombre1
-nombre1()
 
 def mentirOuPas():
+    while
     print('Veux-tu garder ton nombre ? ')
-    ouiNon = input('(O)UI ou (N)ON')
-    if ouiNon != 'O' or ouiNon != 'N':
+    choix = input('(O)UI ou (N)ON')
+    if choix == 'O':
+        print('Tu gardes: ' + (result))
+    elif choix == 'N':
+        print('Voici votre nouveau nombre: ' + nombre1())
+    elif choix != 'O' and choix != 'N':
         print('Mauvaise saisie')
-    elif ouiNon == 'O':
-        print(nombre1)
-    elif ouiNon == 'N':
         nombre1()
 
+while
+nombre1()
 mentirOuPas()
